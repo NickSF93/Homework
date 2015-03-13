c62df90e5ac1c1b294df45ef280eeae5beb4891f
NickSF93 <dc2426@nyu.edu>
2015-03-12 22:46:54 -0400

    commit 4

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -69,11 +69,11 @@ bool ProcessInput(Entity* p1, Entity* p2, SDL_Event& event, const Uint8* keys)
 	}
 
 	//x movement
-	if (keys[SDL_SCANCODE_A] && !p1->collidedLeft)
+	if (keys[SDL_SCANCODE_A])
 	{
 		p1->acceleration_x = -3;
 	}
-	else if (keys[SDL_SCANCODE_D] && !p1->collidedRight)
+	else if (keys[SDL_SCANCODE_D])
 	{
 		p1->acceleration_x = 3;
 	}
@@ -85,7 +85,7 @@ bool ProcessInput(Entity* p1, Entity* p2, SDL_Event& event, const Uint8* keys)
 	//p2
 	if (keys[SDL_SCANCODE_UP] && !p2->inAir)
 	{
-		p2->velocity_y = 3;
+		p2->velocity_y = 3.0;
 		p2->inAir = true;
 	}
 	if (keys[SDL_SCANCODE_LEFT] && !p2->collidedLeft)
@@ -144,34 +144,36 @@ void FixedUpdate(Entity* p1, Entity* p2, vector<Entity*> statics, float elapsed)
 	{
 		if (p1->collided(statics[i]))
 		{
-			if (p1->collidedBottom)
+
+			if (p1->y > statics[i]->y && (p1->x < statics[i]->x + statics[i]->width / 2 && p1->x > statics[i]->x - statics[i]->width / 2))
 			{
+				float yPen = fabs(p1->y - (statics[i]->y) - p1->height / 2 - statics[i]->height / 2);
+				p1->y += yPen;
+				p2->velocity_y = 4;
+				p1->inAir = false;
 			}
-			if (p1->collidedLeft)
+			if (p1->y > statics[i]->y)
 			{
-			}
-			if (p1->collidedRight)
-			{
-				float xPen = fabs(.00001 - p1->width / 2 - statics[i]->width / 2);
-				p1->x -= xPen;
-			}
-			if (p1->collidedTop)
-			{
+				float yPen = fabs(p1->y - (statics[i]->y) - p1->height / 2 - statics[i]->height / 2);
+				p1->y -= yPen;
+				p2->velocity_y = 4;
+				p1->collidedTop = true;
 			}
 		}
-		if (p2->collided(statics[i]))
+	if (p2->collided(statics[i]))
 		{
-			if (p2->collidedBottom)
+
+			if (p2->y > statics[i]->y && (p2->x < statics[i]->x + statics[i]->width / 2 && p2->x > statics[i]->x - statics[i]->width / 2))
 			{
+				float yPen = fabs(p1->y - (statics[i]->y) - p1->height / 2 - statics[i]->height / 2);
+				p2->y += yPen;
+				p2->inAir = false;
 			}
-			if (p2->collidedLeft)
+			if (p2->y > statics[i]->y)
 			{
-			}
-			if (p2->collidedRight)
-			{
-			}
-			if (p2->collidedTop)
-			{
+				float yPen = fabs(p2->y - (statics[i]->y) - p2->height / 2 - statics[i]->height / 2);
+				p2->y -= yPen;
+				p2->collidedTop = true;
 			}
 		}
 	}
@@ -180,13 +182,57 @@ void FixedUpdate(Entity* p1, Entity* p2, vector<Entity*> statics, float elapsed)
 	p1->x += p1->velocity_x * elapsed;
 	p2->x += p2->velocity_x * elapsed;
 
-	
-	if (p1->y <= -.8) // make collided bottom
+	//checks for collisions and adjusts for penetrations
+	//with player
+
+	//with statics
+	for(size_t i = 0; i < statics.size(); i++)
+	{
+		if (p1->collided(statics[i]))
+		{
+
+			if (p1->x > statics[i]->x) // p1 on the right
+			{
+				float xPen = fabs(p1->x - (statics[i]->x) - p1->width / 2 - statics[i]->width / 2);
+				p1->x += xPen;
+				p1->collidedLeft = true;
+			}
+			else if (p1->x < statics[i]->x) // on left
+			{
+				float xPen = fabs(p1->x - (statics[i]->x) - p1->width / 2 - statics[i]->width / 2);
+				p1->x -= xPen;
+				p1->velocity_x = 0;
+				p1->collidedRight = true;
+			}
+		}
+
+		if (p2->collided(statics[i]))
+		{
+
+			if (p2->x > statics[i]->x) // p1 on the right
+			{
+				float xPen = fabs(p2->x - (statics[i]->x) - p2->width / 2 - statics[i]->width / 2);
+				p2->x += xPen;
+				p2->collidedLeft = true;
+			}
+			else if (p1->x < statics[i]->x) // on left
+			{
+				float xPen = fabs(p2->x - (statics[i]->x) - p2->width / 2 - statics[i]->width / 2);
+				p2->x -= xPen;
+				p2->velocity_x = 0;
+				p2->collidedRight = true;
+			}
+		}
+	}
+
+
+
+	if (p1->y <= -.81) // make collided bottom
 	{
 		p1->inAir = false;
 		p1->velocity_y = 0;
 	}
-	if (p2->y <= -.8)
+	if (p2->y <= -.81)
 	{
 		p2->inAir = false;
 		p2->velocity_y = 0;
@@ -233,17 +279,25 @@ int main(int argc, char *argv[])
 	player1->rotation = 0;
 	player1->y = .3;
 	player1->inAir = true;
-	Entity* player2 = new Entity();
+	player1->collidedBottom = false;
+	player1->collidedLeft = false;
+	player1->collidedRight = false;
+	player1->collidedTop = false;
 
+	Entity* player2 = new Entity();
 	player2->textureID = LoadTexture("paddleRed.png");
 	player2->height = .3;
 	player2->width = .1;
 	player2->rotation = 0;
-	player2->friction_x = 3;
+	player2->friction_x = 3.0;
 	player2->x = .9;
 	player2->y = 0;
 	player2->collidedBottom = false;
 	player2->inAir = true;
+	player2->collidedBottom = false;
+	player2->collidedLeft = false;
+	player2->collidedRight = false;
+	player2->collidedTop = false;
 
 	vector<Entity*> statics;
 	float staticX = -1;
@@ -267,7 +321,7 @@ int main(int argc, char *argv[])
 	blockL->inAir = false;
 	blockL->textureID = LoadTexture("paddleBlu.png");
 	blockL->x = -.4;
-	blockL->y = -.7;
+	blockL->y = -.8;
 	statics.push_back(blockL);
 
 	Entity* blockR = new Entity();
@@ -277,7 +331,7 @@ int main(int argc, char *argv[])
 	blockR->inAir = false;
 	blockR->textureID = LoadTexture("paddleBlu.png");
 	blockR->x = .4;
-	blockR->y = -.7;
+	blockR->y = -.8;
 	statics.push_back(blockR);
 	
 	while (ProcessInput(player1, player2, event, keys))
@@ -291,9 +345,10 @@ int main(int argc, char *argv[])
 		}
 		while (fixedElapsed >= FIXED_TIMESTEP) {
 			fixedElapsed -= FIXED_TIMESTEP;
-			//FixedUpdate(player1, player2, fixedElapsed);
+			FixedUpdate(player1, player2, statics, fixedElapsed);
 		}
-		FixedUpdate(player1, player2, statics, fixedElapsed);
+		timeLeftOver = fixedElapsed;
+		//FixedUpdate(player1, player2, statics, fixedElapsed);
 		Render(player1, player2, statics);
 	}
 	return 0;
